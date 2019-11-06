6c72813c380c271ac4c9421717dc58fd9e693a9f
maffic <maffic@rambler.ru>
2019-11-06 10:28:35 +0300

    Подлкючил Spring Cache

--- src/main/java/com/test/Persons/PersonsApplication.java
+++ src/main/java/com/test/Persons/PersonsApplication.java
@@ -2,11 +2,31 @@ package com.test.Persons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cache.Cache;
 
+import java.util.Arrays;
+
+@EnableCaching
 @SpringBootApplication
 public class PersonsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonsApplication.class, args);
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+
+		Cache cache = new ConcurrentMapCache("byPersonFirstName");
+
+		SimpleCacheManager manager = new SimpleCacheManager();
+		manager.setCaches(Arrays.asList(cache));
+
+		return manager;
 	}
 }
--- src/main/java/com/test/Persons/Repository/PersonRepository.java
+++ src/main/java/com/test/Persons/Repository/PersonRepository.java
@@ -1,6 +1,8 @@
 package com.test.Persons.Repository;
 
 import com.test.Persons.model.Person;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,13 @@ import java.util.List;
 @Repository
 @RepositoryRestResource(collectionResourceRel = "person", path = "person")
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
     List<Person> findByLastName(String last_name);
+
+    @Cacheable("byPersonFirstName")
     List<Person> findByFirstName(String first_name);
+
+    @Override
+    @CacheEvict(value = "byPersonFirstName", key = "#p0.firstName")
+    <S extends Person> S save(S entity);
 }
