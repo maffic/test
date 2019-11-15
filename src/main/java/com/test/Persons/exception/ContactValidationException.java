a869a9a8d86328211d360113a5aa2b8cee8631e7
Maffic <maksim@foliforov.ru>
2019-11-15 11:45:16 +0300

    *Исправил варнинг

--- src/main/java/com/test/Persons/exception/ContactValidationException.java
+++ src/main/java/com/test/Persons/exception/ContactValidationException.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ContactValidationException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
     @Getter
     private String value;
     @Getter
--- src/main/java/com/test/Persons/exception/PersonNotfoundException.java
+++ src/main/java/com/test/Persons/exception/PersonNotfoundException.java
@@ -3,6 +3,8 @@ package com.test.Persons.exception;
 import lombok.Getter;
 
 public class PersonNotfoundException extends RuntimeException {
+    
+    private static final long serialVersionUID = 2L;
 
     @Getter
     private Long id;
