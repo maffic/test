109ef42c9ae1de5cb38b9cecc535170511cdbb69
Maffic <maksim@foliforov.ru>
2019-11-15 11:41:04 +0300

    *Ошибка автосвязывания

--- src/main/java/com/test/Persons/Controllers/ContactController.java
+++ src/main/java/com/test/Persons/Controllers/ContactController.java
@@ -1,40 +1,25 @@
 package com.test.Persons.Controllers;
 
-import com.test.Persons.Repository.ContactRepository;
-import com.test.Persons.Repository.PersonRepository;
-import com.test.Persons.exception.ContactValidationException;
-import com.test.Persons.exception.PersonNotfoundException;
-import com.test.Persons.model.Contact;
-import com.test.Persons.model.ContactType;
-import com.test.Persons.model.Person;
 import com.test.Persons.services.ContactServices;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
 
-    @Autowired
-    ContactServices contactServices;
-
     @PostMapping("{Id}/add/addr")
     public ResponseEntity<?> addContactAddr(@PathVariable Long Id, @RequestBody String value) {
-        return contactServices.addAddr(Id, value);
+        return new ContactServices().addAddr(Id, value);
     }
 
     @PostMapping("{Id}/add/email")
     public ResponseEntity<?> addContactEmail(@PathVariable Long Id, @RequestBody String value) {
-        return contactServices.addEmail(Id, value);
+        return new ContactServices().addEmail(Id, value);
     }
 
     @PostMapping("{Id}/add/phone")
     public ResponseEntity<?> addContactPhone(@PathVariable Long Id, @RequestBody String value) {
-        return contactServices.addPhone(Id, value);
+        return new ContactServices().addPhone(Id, value);
     }
 }
